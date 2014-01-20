c6b653a9a07155526c2df056e5dca28f2ba109e2
Ben Friedland <ben174@gmail.com>
2014-01-20 14:30:36 -0800

    Basic automation working.

--- automate.py
+++ automate.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
         'bedroom': [5],
         'stairs': [4,6],
         'house': range(1,9),
+    }
 
     if 'brightness' in line:
         command = 'dim'
@@ -44,20 +45,25 @@ if __name__ == '__main__':
     elif 'stairs' in line:
         room = 'stairs'
 
-    print result
-    print result[0]
-    full_command = result[0].split()
-    room, _, command = full_command
-
-    office = [7,8,9]
-    living_room = [6,3,1]
-    
-    command_on = (command == 'on')
-    if room == 'office':
-        for light_index in office:
-            b.set_light(light_index,'on', command_on)
+    if 'on' in line:
+        command = 'power'
+        value = 'on'
+    elif 'off' in line:
+        command = 'power'
+        value = 'off'
 
 
+    print 'Room: %s, Command: %s, Value: %s' % (room, command, value)
+
+    if not (room and command and value):
+        print 'not enough info'
+
+
+    if command == 'power':
+        light_state = (value == 'on')
+        for light_index in light_groups[room]:
+            print 'Setting light %s: %s' % (str(light_index), str(light_state))
+            b.set_light(light_index,'on', light_state)
 
 
 
