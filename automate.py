15b29f286b429fe98fae380347b407b042ea2168
Ben Friedland <ben174@gmail.com>
2014-01-20 16:10:49 -0800

    Documented automate.py

--- automate.py
+++ automate.py
@@ -1,3 +1,12 @@
+''' automate.py  - Written by Ben Friedland
+
+    A tool to automate my house via voice commands. Accepts commands like:
+        'Turn office lights on'
+        'Turn upstairs lights blue'
+        'Set temperature to 68'
+        'Turn living room into a rainbow.'
+'''
+
 from pygsr import Pygsr
 from phue import Bridge
 import time
@@ -5,6 +14,9 @@ import settings
 
 
 def parse_command(line):
+    ''' Takes a line of english text and tries to interpret it into a command
+
+    '''
     b = Bridge(settings.HUE_BRIDGE)
     b.connect()
     b.get_api()
@@ -29,7 +41,7 @@ def parse_command(line):
         'kitchen': [2],
         'bedroom': [5],
         'stairs': [4, 6],
-        'house': range(1, 9),
+        'house': range(1, 10),
     }
 
     if 'brightness' in line_split:
@@ -70,11 +82,19 @@ def parse_command(line):
     if not (command and value):
         return False
 
+    if not room:
+        print 'You didn\'t specify a room. Assuming the whole house.'
+        room = 'house'
+
     if command == 'power':
         light_state = (value == 'on')
         for light_index in light_groups[room]:
             print 'Setting light %s: %s' % (str(light_index), str(light_state))
             b.set_light(light_index, 'on', light_state)
+            if light_state:
+                # reset color
+                b.set_light(light_index, 'hue', 15331)
+                b.set_light(light_index, 'sat', 121)
         return True
     elif command == 'hue':
         for light_index in light_groups[room]:
@@ -93,6 +113,9 @@ def parse_command(line):
 
 
 def set_temperature(degrees):
+    ''' Sets temperature of nest thermostat to specified degrees.
+
+    '''
     from nest import Nest
     nest = Nest(username=settings.NEST_LOGIN, password=settings.NEST_PASS)
     nest.login()
@@ -101,11 +124,28 @@ def set_temperature(degrees):
 
 
 def record_command():
+    ''' Records audio and sends it to google to translate to text
+
+    '''
     speech = Pygsr()
     speech.record(3, 1)
     result = speech.speech_to_text('en-US')
     line = result[0].lower()
     return line
+
+
+def get_office_lights():
+    ''' Gets the current state of my office lights so I know what values 
+    to plug in to presets.
+
+    '''
+    b = Bridge(settings.HUE_BRIDGE)
+    b.connect()
+    b.get_api()
+    light_index = 9
+    print b.get_light(light_index, 'on')
+    print b.get_light(light_index, 'hue')
+    print b.get_light(light_index, 'sat')
 
 
 if __name__ == '__main__':
