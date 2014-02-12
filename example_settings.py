1c992cef742c7822c3eaa89e824061b7bcbbda9b
Ben Friedland <ben174@gmail.com>
2014-02-12 10:43:08 -0800

    New example settings.

--- example_settings.py
+++ example_settings.py
@@ -12,15 +12,17 @@ COLORS = {
 
 LIGHT_GROUPS = {
     'office': [7, 8, 9],
-    'living': [6, 3, 1],
+    'living': [6, 3, 1, 13, 15],
     'upstairs': [4, 5],
+    'downstairs': [6, 3, 1, 2, 12, 7, 8, 9, 13, 15],
     'kitchen': [2],
     'bedroom': [5, 11],
     'stairs': [4, 6],
     'staircase': [4, 6],
     'hallway': [12],
     'guest': [10],
-    'house': range(1, 13),
+    'porch': [14],
+    'house': range(1, 16),
 }
 
 DEFAULT_ROOM = 'house'
@@ -28,4 +30,14 @@ DEFAULT_ROOM = 'house'
 FLAC_CONVERT = 'sox audio -t wav -r 48000 -t flac audio.flac'
 # or if you want to use flac command use 'flac -f audio'
 
-AUDIO_DEVICE = 'pulse'  # sysdefault, pulse
+AUDIO_DEVICE = 'pulse'  # omit to auto discover
+
+RECORD_LENGTH = 3
+
+LANGUAGE = 'en-US'
+
+# not yet implemented
+SUBSTITUTIONS = {
+    'green': ['creed', 'screen'],
+    'read': ['red', 'screen']
+}
