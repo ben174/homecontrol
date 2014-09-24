e43d234afe5f03b4b532bc6e7818f00829ea1786
Ben Friedland <ben174@gmail.com>
2014-09-23 17:32:03 -0700

    Remote module basic functions.

--- remote.py
+++ remote.py
@@ -1,11 +1,18 @@
+import sys
 from control import Control
 from command import Command
 
 def main():
+    print sys.argv
+    arg = sys.argv[1]
     command = Command()
-    command.room='office'
+    command.room='living'
     command.action='power'
     command.value='on'
+    if arg == '1':
+    	command.value = 'on'
+    elif arg == '2':
+    	command.value = 'off'
     control = Control()
     control.execute_command(command)
 
